b27e73dfe560e7c603a46a17e2e2859ba36de12d
Anton Wiberg <anton.wiberg@liu.se>
2023-03-21 09:52:00 +0100

    fixes on examples

--- examples/snake_cube.py
+++ examples/snake_cube.py
@@ -2,7 +2,7 @@ import obpgenerator
 
 file_path1 = "examples\layer_nine_cubes.svg"
 file_path2 = "examples\layer_15x15_cube.svg"
-paths = obpgenerator.file_import.import_svg_layer(file_path2)
+paths = obpgenerator.file_import.import_svg_layer(file_path1)
 my_layer = obpgenerator.Layer.Layer()
 my_layer.create_from_mplt_paths(paths)
 my_layer.set_shapes(1)
