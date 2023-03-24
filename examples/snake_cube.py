40b9b3efb9a6ab3fa3ad62af8f0188abc285459b
Anton Wiberg <anton.wiberg@liu.se>
2023-03-24 16:56:10 +0100

    new build

--- examples/snake_cube.py
+++ examples/snake_cube.py
@@ -14,7 +14,7 @@ manufacturing_settings.set_spot_size(0.1,lower=0.1,upper=1)
 manufacturing_settings.set_beam_power(1500,lower=1000,upper=2000)
 manufacturing_settings.set_scan_speed(1,lower=1,upper=2)
 manufacturing_settings.set_dwell_time(100,lower=50,upper=150)
-ramp_settings = dict(ramp_beam_power=1,ramp_dwell_time=0,ramp_scan_speed=0,ramp_spot_size=0)
+ramp_settings = dict(ramp_beam_power=0,ramp_dwell_time=0,ramp_scan_speed=1,ramp_spot_size=0)
 
 
 my_layer.set_manufacturing_settings(manufacturing_settings)
--- obpgenerator/generate_obp.py
+++ obpgenerator/generate_obp.py
@@ -4,7 +4,7 @@ import obpgenerator.manufacturing_settings as settings
 def generate_lines(elements,set):
     lines = []
     for element in elements:
-        lines.append(obp.Line(element[0], element[1], set.scan_speed.value, set.get_beam_parameters()))
+        lines.append(obp.Line(element[0], element[1], int(set.scan_speed.value), set.get_beam_parameters()))
     return lines
 
 def generate_points(elements,set):
@@ -19,5 +19,5 @@ def generate_points(elements,set):
 def generate_curves(elements, set):
     curves = []
     for element in elements:
-        curves.append(obp.Curve(element[0],element[1],element[3],element[0], set.scan_speed.value, set.get_beam_parameters()))
+        curves.append(obp.Curve(element[0],element[1],element[3],element[0], int(set.scan_speed.value), set.get_beam_parameters()))
     return curves
