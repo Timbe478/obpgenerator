939fb10f27a4eb9e211519286035b7a5bb3f8625
Anton Wiberg <anton.wiberg@liu.se>
2023-04-24 14:46:22 +0200

    updated 3D example

--- examples/example_3D.py
+++ examples/example_3D.py
@@ -2,13 +2,11 @@ import obpgenerator.Part as Part
 import obpgenerator.slicer as slicer
 import obpgenerator.manufacturing_settings as manufacturing_settings
 
-stl_file = ["examples\\two_cubes.stl", "examples\\cube10_10_10.stl"]
+stl_file = ["examples\\cube1.stl", "examples\\cube2.stl"]
 slices = slicer.slice_stl(stl_file,1)
 my_part = Part.Part()
 my_part.create_from_mplt_paths(slices)
 
 manuf = manufacturing_settings.ManufacturingSetting()
 my_part.set_layers(1, manuf, size=60, angle_between_layers=0, melt_strategy="point_random", nmb_of_scans=1, sorting_strategy="shapes_first")
-my_part.create_obps()
-#print(my_part.layers[0].shapes)
 my_part.export_build_file(r"C:\Users\antwi87\Downloads\obp_files")
