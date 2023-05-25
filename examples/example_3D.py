bdff4552335d089df57c5263690057d0d53d5168
Anton Wiberg <anton.wiberg@liu.se>
2023-05-25 11:56:01 +0200

    Updated example for 3D

--- examples/example_3D.py
+++ examples/example_3D.py
@@ -2,11 +2,23 @@ import obpgenerator.Part as Part
 import obpgenerator.slicer as slicer
 import obpgenerator.manufacturing_settings as manufacturing_settings
 
-stl_file = ["examples\\cube1.stl", "examples\\cube2.stl"]
-slices = slicer.slice_stl(stl_file,0.1)
+stl_files = ["examples\\cube1.stl", "examples\\cube2.stl"]
+slices = slicer.slice_stl(stl_files,0.15) #slices the stl files with a layer height of 0.15 mm
 my_part = Part.Part()
 my_part.create_from_mplt_paths(slices)
 
-manuf = manufacturing_settings.ManufacturingSetting()
-my_part.set_layers(0.1, manuf, size=60, angle_between_layers=10, melt_strategy="point_random", nmb_of_scans=1, sorting_strategy="shapes_first")
-my_part.export_build_file(r"C:\Users\antwi87\Downloads\obp_files")
+#Sets two different manufacturing settings for the two geometries
+settings1 = manufacturing_settings.ManufacturingSetting()
+settings1.set_spot_size(0.5) #[µm]
+settings1.set_beam_power(1000) #[W]
+settings1.set_dwell_time(1) #[ns]
+settings1.set_scan_speed(12) #[micrometers/second] 
+settings2 = manufacturing_settings.ManufacturingSetting()
+settings1.set_spot_size(0.75) #[µm]
+settings1.set_beam_power(1500) #[W]
+settings1.set_dwell_time(1) #[ns]
+settings1.set_scan_speed(10) #[micrometers/second] 
+
+my_part.set_layers(0.15, settings1, size=60, angle_between_layers=10, melt_strategy="point_random", nmb_of_scans=1, sorting_strategy="shapes_first") #First we just set one manufacturing setting
+my_part.set_settings(manufacturing_settings = [settings1, settings2], melt_strategies=["line_snake","point_random"]) #We can then set different settings for the seperate geometries
+my_part.export_build_file(r"C:\Users\antwi87\Downloads\obp_files", export_shapes_individual=False) #Exports the build file (each layer will be one obp file, if export_shapes_individual=True each geoemtry will be in seperate obp files)
